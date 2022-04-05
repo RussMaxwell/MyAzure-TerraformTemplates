b921d994f8d5f484626e9d8143b6b29fa299b357
Russ Maxwell <russmaxwell@gmail.com>
2022-04-05 13:41:11 -0400

    add webapp pulling image from docker hub

---
+++ AZ_WebApp_from_DockerImage/main.tf
@@ -0,0 +1,61 @@
+locals {
+    subscription_id    = "<insertSubscriptionID>"
+    tenant_id          = "<insertTenantID>"
+    app-location       = "eastus"
+    app-resource-group = "Contoso-App-rg"
+    webApp-Name        = "contosodog"
+}
+
+
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = ">=2.99.0"
+    }
+  }
+}
+
+
+provider "azurerm" {
+  # Configuration options
+  subscription_id = local.subscription_id
+  tenant_id       = local.tenant_id
+  features {}
+}
+
+
+resource "azurerm_resource_group" "ContosoApp-RG" {
+  name     = local.app-resource-group
+  location = local.app-location
+}
+
+
+resource "azurerm_service_plan" "Contoso-ServicePlan" {
+  name                = "contosoASP"
+  resource_group_name = azurerm_resource_group.ContosoApp-RG.name
+  location            = azurerm_resource_group.ContosoApp-RG.location
+  sku_name            = "B1"
+  os_type             = "Linux"
+}
+
+
+resource "azurerm_linux_web_app" "Contoso-WebApp" {
+  name                = local.webApp-Name
+  resource_group_name = azurerm_resource_group.ContosoApp-RG.name
+  location            = azurerm_service_plan.Contoso-ServicePlan.location
+  service_plan_id     = azurerm_service_plan.Contoso-ServicePlan.id
+
+  identity {
+      type = "SystemAssigned"
+  }
+
+  site_config {
+      container_registry_use_managed_identity = true
+
+      application_stack {
+      docker_image = "russmax/testwebapp"
+      docker_image_tag = "latest"
+      }
+    }
+}
